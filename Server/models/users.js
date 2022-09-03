6a77129631d837206a03a45f5fb7243ee80d09b6
PhanNhan123 <ptnhan10091999@gmail.com>
2022-09-03 13:40:34 +0700

    user_model

--- Server/models/users.js
+++ Server/models/users.js
@@ -35,7 +35,8 @@ const userSchema = new Schema({
     },
     avatar:{
         type: String,
-        required: true
+        require: true,
+        default: null
     },
     token:{
         type: String,
